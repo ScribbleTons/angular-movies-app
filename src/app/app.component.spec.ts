47df8504ce2bf4ff161d04ff3606244ead17a16c
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2023-03-24 06:41:29 +0100

    update project title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fake-estate'`, () => {
+  it(`should have as title 'movie-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fake-estate');
+    expect(app.title).toEqual('movie-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fake-estate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'movie-app app is running!'
+    );
   });
 });
